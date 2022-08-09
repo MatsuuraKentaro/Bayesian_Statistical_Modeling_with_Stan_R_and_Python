7964d0d4d2675e801e0988ec3cbcc44a8872f4b6
ceiiinosssttuv@gmail.com <ceiiinosssttuv@gmail.com>
2022-08-09 17:52:18 +0900

    removed the old version of Fig. 13.3 and renamed Fig. 13.4 to Fig. 13.3.

--- chap13/fig13-3.R
+++ chap13/fig13-3.R
@@ -1,24 +1,22 @@
+library(dplyr)
 library(ggplot2)
 
-x <- seq(0, 1, len=201)
-y <- dbeta(x, shape1=2, shape2=4)
-d <- data.frame(x=x*610, y=y)
+load('output/result-model13-3.RData')
+times_plot <- c(1,7,14,20)
+# times_plot <- 1:20
+d_dat <- d_dat %>% mutate(time_lab = factor(paste0('time = ', time), levels=paste0('time = ', times_plot))) %>% filter(time %in% times_plot)
+d_pre <- d_pre %>% mutate(time_lab = factor(paste0('time = ', time), levels=paste0('time = ', times_plot))) %>% filter(time %in% times_plot)
+d_est <- d_est %>% mutate(time_lab = factor(paste0('time = ', time), levels=paste0('time = ', times_plot))) %>% filter(time %in% times_plot)
+d_sel <- d_sel %>% mutate(time_lab = factor(paste0('time = ', time), levels=paste0('time = ', times_plot))) %>% filter(time %in% times_plot)
 
-X <- c(89, 144, 233, 377)/610
-Y <- dbeta(X, shape1=2, shape2=4)
-d1 <- data.frame(x=X*610, y=Y)
-
-p <- ggplot(data=d, aes(x, y)) +
-  theme_classic() +
-  theme(text=element_text(size=22),
-        axis.title.y=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank()) +
-  geom_ribbon(data=subset(d, x > 377), aes(x=x, y=y, ymin=0, ymax=y), alpha=0.4) +
-  geom_line(color='black', size=2, alpha=0.4) +
-  geom_segment(data=d1, aes(x=x, xend=x, y=0, yend=y), linetype='11', size=2) +
-  geom_point(data=d1, aes(x=x, y=y), size=5) +
-  scale_x_continuous(breaks=c(0, 89, 144, 233, 377, 610)) +
-  labs(x='')
-ggsave(p, file='output/fig13-3.png', dpi=300, w=6, h=3)
-
+p <- ggplot() +
+  theme_bw(base_size=20) +
+  facet_wrap(~time_lab) +
+  geom_ribbon(data=d_est, aes(x=X, ymin=`2.5%`, ymax=`97.5%`), alpha=0.4) +
+  geom_line(data=d_est, aes(x=X, y=`50%`), color='black') +
+  geom_point(data=d_pre, aes(x=X, y=Y), size=3, color='black', shape=1) +
+  geom_point(data=d_dat, aes(x=X, y=Y), size=3, color='black', shape=16) +
+  geom_vline(data=d_sel, aes(xintercept=X), size=0.5, color='red', linetype='dashed', alpha=0.7) +
+  geom_point(data=d_sel, aes(x=X), y=5, size=3, color='red', shape=17) +
+  labs(x='X', y='Y')
+ggsave(p, file='output/fig13-3.png', dpi=300, w=8, h=6)
