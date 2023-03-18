695b9961035615d0bedb157d1d2a6db60f026367
Kentaro Matsuura <matsuura@mail.hoxo-m.com>
2023-03-18 16:36:31 +0900

    modified the width of indentation

--- chap04/fig4-1.py
+++ chap04/fig4-1.py
@@ -3,6 +3,6 @@ from plotnine import ggplot, aes, theme_bw, geom_point
 
 d = pandas.read_csv('input/data-salary.csv')
 p = (ggplot(d, aes('X', 'Y'))
- + theme_bw(base_size=18)
- + geom_point(shape='o', size=3))
+    + theme_bw(base_size=18)
+    + geom_point(shape='o', size=3))
 p.save(filename='output/fig4-1.py.png', dpi=300, width=4, height=3)
