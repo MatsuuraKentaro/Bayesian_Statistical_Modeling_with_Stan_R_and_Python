25dfd26ca299b31c99929a2305afb8c55190d924
Kentaro Matsuura <matsuura@mail.hoxo-m.com>
2023-03-19 16:21:41 +0900

    small fix for the legend

--- chap07/fig7-8.py
+++ chap07/fig7-8.py
@@ -1,15 +1,18 @@
 import pandas
-from plotnine import ggplot, aes, theme_bw, geom_point, scale_shape_manual
+from plotnine import ggplot, aes, theme_bw, geom_point, scale_shape_manual, labs
 
 d = pandas.read_csv('input/data-50m.csv')
 
 p = (ggplot(d, aes('Weight', 'Y'))
     + theme_bw(base_size=18)
-    + geom_point(shape='o', size=2))
+    + geom_point(shape='o', size=2)
+)
 p.save(filename='output/fig7-8-left.py.png', dpi=300, width=3.8, height=3)
 
 p = (ggplot(d, aes('Weight', 'Y', shape='factor(Age)'))
     + theme_bw(base_size=18)
     + geom_point(size=2)
-    + scale_shape_manual(values=[3, 1, 2, 4, 5, 6]))
+    + scale_shape_manual(values=['+', 'o', '^', 'x', 'D', 'v'])
+    + labs(shape='Age')
+)
 p.save(filename='output/fig7-8-right.py.png', dpi=300, width=4.5, height=3)
